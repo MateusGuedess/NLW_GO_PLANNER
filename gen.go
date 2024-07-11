e0e70759f637f6a783c34d0d2d5bde0bee3d8d39
Mateus Guedes <mxteusgued@gmail.com>
2024-07-11 18:58:59 -0300

    add sqlc command to go generate

--- gen.go
+++ gen.go
@@ -1,3 +1,4 @@
 package mai
 
 //go:generate tern migrate --migrations ./internal/pgstore/migrations --config ./internal/pgstore/migrations/tern.conf
+//go:generate sqlc generate -f ./internal/pgstore/sqlc.yaml
